35df47d9e72c6c7482585d8384df12ca13c3276e
Matthew Randle <exportknowledge.matthew@gmail.com>
2020-04-24 20:22:40 +0100

    Added prod.js

--- backend/services/db.js
+++ backend/services/db.js
@@ -1,31 +1,14 @@
 const mysql = require("mysql");
 const keys = require("../../config/keys");
-const fs = require("fs");
 
 var pool;
 
-if(process.env.NODE_ENV === "development") {
-    pool = mysql.createPool({
-        host: keys.databaseHost,
-        user: keys.databaseUser,
-        password: keys.databasePassword,
-        database: keys.database,
-        charset: "utf8mb4"
-    });
-}
-else {
-    pool = mysql.createPool({
-        host: keys.databaseHost,
-        user: keys.databaseUser,
-        password: keys.databasePassword,
-        database: keys.database,
-        charset: "utf8mb4"/* ,
-        ssl: {
-            ca: fs.readFileSync(keys.mysqlCA),
-            key: fs.readFileSync(keys.mysqlKey),
-            cert: fs.readFileSync(keys.mysqlCert)
-        } */
-    });
-}
+pool = mysql.createPool({
+    host: keys.databaseHost,
+    user: keys.databaseUser,
+    password: keys.databasePassword,
+    database: keys.database,
+    charset: "utf8mb4"
+});
 
 module.exports = pool;
--- config/prod.js.js
+++ config/prod.js.js
@@ -1,11 +1,8 @@
 module.exports = {
-    cookieKey: "gdfgre54y45y54yh54ygh5g54y53y35y345y",
-    databaseHost: "localhost", 
-    databaseUser: "root", 
-    databasePassword: "Mahjkrmahjkr9871", 
-    database: "recommender",
-    TMDBKey: "abed60834d8a74d3044fac789f6c7c07",
-    mysqlCA: "",
-    mysqlKey: "",
-    mysqlCert: ""
+    cookieKey: process.env.COOKIE_KEY,
+    databaseHost: process.env.DATABASE_HOST, 
+    databaseUser: process.env.DATABASE_USER, 
+    databasePassword: process.env.DATABASE_PASSWORD, 
+    database: process.env.DATABASE,
+    TMDBKey: process.env.TMDB_KEY
 };
